d5003c3f00a2bbe11af070c3e3e7853665419cce
Inquis1t0r <pi0tr3k9292@gmail.com>
2019-11-11 11:13:51 +0100

    Update IsitTimeforMilkandCookies?.cs

--- C#/IsitTimeforMilkandCookies?.cs
+++ C#/IsitTimeforMilkandCookies?.cs
@@ -1,7 +1,11 @@
+using System;
+
 public class Program
 {
     public static bool TimeForMilkAndCookies(int year, int month, int day)
     {
-    //TODO
+			DateTime compareDate = new DateTime(year, month, day);
+
+			return compareDate.Day == 24 && compareDate.Month == 12;
     }
 }
