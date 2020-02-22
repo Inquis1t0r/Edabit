e9582ece9b04e045ec573b0704bb76fa558a6c02
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-02-22 18:38:46 +0100

    Create RemovetheFirstandLastCharacters.cs

---
+++ C#/RemovetheFirstandLastCharacters.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public static string RemoveFirstLast(string str)
+    {
+			if(str.Length <= 2)
+				return str;
+			else
+			return str.Substring(1, str.Length -2);
+    }
+}
