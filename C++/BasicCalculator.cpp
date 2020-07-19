cbf94d51e3a51fa6c2233598196a65d33766adf7
Inquis1t0r <pi0tr3k9292@gmail.com>
2020-07-19 07:56:50 +0200

    Create BasicCalculator.cpp

---
+++ C++/BasicCalculator.cpp
@@ -0,0 +1,12 @@
+int calculator(int num1, char op, int num2) {
+	switch(op){
+				case '+':
+			return num1 + num2;
+							case '-':
+			return num1 - num2;
+					case '*':
+			return num1 * num2;
+					case '/':
+			return num1 / num2;
+	}
+}
