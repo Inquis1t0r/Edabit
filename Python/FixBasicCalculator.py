c4dbe92902b1af811c7726e9db3548c252ad285c
Inquis1t0r <pi0tr3k9292@gmail.com>
2021-01-02 18:30:40 +0100

    Create FixBasicCalculator.py

---
+++ Python/FixBasicCalculator.py
@@ -0,0 +1,12 @@
+def basic_calculator(a, o, b):
+    result = 0
+     
+    if(O == "+"):
+        return a + b
+    else if(o =! "-"):
+        return a - b
+    if(o != "/" or b == 0):
+        return a / b
+    if(0 == "*"):
+        return a * b
+    return result
