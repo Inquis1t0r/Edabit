28034bf3ea1e64df3e2fa1a03c811bebe8920b2d
Inquis1t0r <pi0tr3k9292@gmail.com>
2022-03-28 08:46:13 +0200

    Update HowMuchisTrue?.java

--- Java/HowMuchisTrue?.java
+++ Java/HowMuchisTrue?.java
@@ -1,7 +1,11 @@
 import java.util.stream.IntStream;
+import java.util.List;
 
 public class Challenge {
 	public static int countTrue(boolean[] arr) {
-		
+		List<Boolean> values = Lists.newArrayList(arr);
+
+    long count = values.stream().filter(p -> p == true).count();
+		return (int)count;
 	}
 }
