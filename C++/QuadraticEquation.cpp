a723d1b347a8a0ca4e785c57418685862cd59cf4
Inquis1t0r <pi0tr3k9292@gmail.com>
2023-04-15 19:39:43 +0200

    Update QuadraticEquation.cpp

--- C++/QuadraticEquation.cpp
+++ C++/QuadraticEquation.cpp
@@ -1,3 +1,7 @@
+#include <cmath>
+
 int quadraticEquation(int a, int b, int c) {
-	
+    int discriminant = b*b - 4*a*c;  // calculate the discriminant
+    int root = (-b + sqrt(discriminant)) / (2*a);  // calculate the root with the + sign
+    return root;
 }
