c95307befbf4d87a6aa982aed8643521763278e1
Inquis1t0r <pi0tr3k9292@gmail.com>
2024-10-08 14:29:54 +0200

    Update OlderThanMe.js

--- JavaScript/OlderThanMe.js
+++ JavaScript/OlderThanMe.js
@@ -4,8 +4,14 @@ class Person {
 		this.age = age;
 	}
 
-	compareAge(other) {
+	  compareAge(other) {
 		// Write code here!
-		
+	    if (this.age > other.age) {
+	      return `${other.name} is younger than me.`;
+	    } else if (this.age < other.age) {
+	      return `${other.name} is older than me.`;
+	    } else {
+	      return `${other.name} is the same age as me.`;
+	    }
 	}
 }
